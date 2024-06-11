cc8239e67eb3105d2c33eb32dfec873a1403e2a5
benbowen <ben.bowen@gmail.com>
2024-06-11 10:54:50 -0700

    various small changes

--- carbon_network/build/data_collection/rsync_files_backup.sh
+++ carbon_network/build/data_collection/rsync_files_backup.sh
@@ -5,6 +5,7 @@ rsync -avm --include='*.h5' --include='*.parquet' --include='*/' --exclude='*' /
 
 ### backup all massive files
 rsync -avm --include='*.h5' --include='*.parquet' --include='*/' --exclude='*' /pscratch/sd/b/bpb/massive/ /global/cfs/cdirs/metatlas/projects/carbon_network/raw_data/massive
+rsync -avm --include='*.parquet' --include='*/' --exclude='*' /pscratch/sd/b/bpb/massive/ /global/cfs/cdirs/metatlas/projects/carbon_network/raw_data/massive
 
 ### backup all "no buddy" metatlas files
-# rsync -avm --dry-run --include='*.parquet' --include='*/' --exclude='*' /pscratch/sd/b/bpb/metatlas_mdm_parquet_files/global/cfs/cdirs/metatlas/raw_data /global/cfs/cdirs/metatlas/projects/carbon_network/raw_data/metatlas_no_buddy/
+rsync -avm --dry-run --include='*.parquet' --include='*/' --exclude='*' /pscratch/sd/b/bpb/metatlas_mdm_parquet_files/global/cfs/cdirs/metatlas/raw_data /global/cfs/cdirs/metatlas/projects/carbon_network/raw_data/metatlas_no_buddy/
